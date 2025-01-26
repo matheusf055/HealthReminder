8e05cb9b10663d29359f331e074aeb5e2c0f0489
Matheus Fernandes <m.cesar.nunes2014@gmail.com>
2025-01-26 00:05:21 -0300

    Adiciona a validação de dados para o dto

--- HealthReminder.AppService/MedicalApointment/DTOs/CreateMedicalAppointmentDto.cs
+++ HealthReminder.AppService/MedicalApointment/DTOs/CreateMedicalAppointmentDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,15 @@ namespace HealthReminder.AppService.MedicalApointment.DTOs
 {
     public class CreateMedicalAppointmentDto
     {
+        [Required(ErrorMessage = "Nome do médico é obrigatório.")]
         public string DoctorName { get; set; }
+
         public string? Specialty { get; set; }
+
+        [Required(ErrorMessage = "Data e hora da consulta são obrigatórias.")]
         public DateTime AppointmentDateTime { get; set; }
+
+        [Required(ErrorMessage = "Localização é obrigatória.")]
         public string Location { get; set; }
     }
 }
