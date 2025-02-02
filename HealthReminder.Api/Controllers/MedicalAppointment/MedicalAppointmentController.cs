7ab35ec627d05aa9482723d9a860dcb97b9b87f7
Matheus Fernandes <m.cesar.nunes2014@gmail.com>
2025-02-02 18:12:17 -0300

    Ajusta o nome da marcação de consulta médica

--- HealthReminder.Api/Controllers/MedicalAppointment/MedicalAppointmentController.cs
+++ HealthReminder.Api/Controllers/MedicalAppointment/MedicalAppointmentController.cs
@@ -1,57 +1,58 @@
 ﻿using HealthReminder.AppService.Interfaces.MedicalAppointment;
+using HealthReminder.AppService.MedicalApointment;
 using HealthReminder.AppService.MedicalApointment.DTOs;
 using HealthReminder.Domain.Common;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HealthReminder.Api.Controllers.MedicationAppointment
+namespace HealthReminder.Api.Controllers.MedicalAppointment
 {
     [ApiController]
     [Authorize]
-    [Route("api/medicationAppointment")]
-    public class MedicationAppointmentController : ControllerBase
+    [Route("api/medicalAppointment")]
+    public class MedicalAppointmentController : ControllerBase
     {
-        private readonly IMedicalAppointmentAppService _medicationAppointmentAppService;
+        private readonly IMedicalAppointmentAppService _medicalAppointmentAppService;
         private readonly IUser _user;
 
-        public MedicationAppointmentController(IMedicalAppointmentAppService medicationAppointmentAppService, IUser user)
+        public MedicalAppointmentController(IMedicalAppointmentAppService medicalAppointmentAppService, IUser user)
         {
-            _medicationAppointmentAppService = medicationAppointmentAppService;
+            _medicalAppointmentAppService = medicalAppointmentAppService;
             _user = user;
         }
 
         [HttpPost]
         public async Task<IActionResult> AddMedicalAppointmentAsync(CreateMedicalAppointmentDto createMedicalAppointmentDto)
         {
-            await _medicationAppointmentAppService.AddMedicalAppointmentAsync(createMedicalAppointmentDto, _user);
+            await _medicalAppointmentAppService.AddMedicalAppointmentAsync(createMedicalAppointmentDto, _user);
             return Ok("Consulta criada com sucesso.");
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllByUser()
         {
-            var medicalAppointments = await _medicationAppointmentAppService.GetMedicalAppointmentsByUserIdAsync(_user.Id, _user);
+            var medicalAppointments = await _medicalAppointmentAppService.GetMedicalAppointmentsByUserIdAsync(_user.Id, _user);
             return Ok(medicalAppointments);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetMedicalAppointmentByIdAsync(Guid id)
         {
-            var medicalAppointment = await _medicationAppointmentAppService.GetMedicalAppointmentByIdAsync(id, _user);
+            var medicalAppointment = await _medicalAppointmentAppService.GetMedicalAppointmentByIdAsync(id, _user);
             return Ok(medicalAppointment);
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateMedicalAppointmentAsync(Guid id, [FromBody] UpdateMedicalAppointmentDto updateMedicalAppointmentDto)
         {
-            await _medicationAppointmentAppService.UpdateMedicalAppointmentAsync(id, updateMedicalAppointmentDto, _user);
+            await _medicalAppointmentAppService.UpdateMedicalAppointmentAsync(id, updateMedicalAppointmentDto, _user);
             return Ok("Consulta atualizada com sucesso.");
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteMedicalAppointmentAsync(Guid id)
         {
-            await _medicationAppointmentAppService.DeleteMedicalAppointmentAsync(id, _user);
+            await _medicalAppointmentAppService.DeleteMedicalAppointmentAsync(id, _user);
             return Ok("Consulta deletada com sucesso.");
         }
     }
