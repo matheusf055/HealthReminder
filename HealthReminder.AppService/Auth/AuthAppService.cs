898ed1b319dc511445ae67933071b33645dbc0d1
09042110 <09042110@FMN026-L101-015>
2025-02-20 19:44:48 -0300

    Renomeia o o AuthRepository para UserRepository e adiciona metódos mais alguns métodos para usuário

--- HealthReminder.AppService/Auth/AuthAppService.cs
+++ HealthReminder.AppService/Auth/AuthAppService.cs
@@ -7,29 +7,29 @@ namespace HealthReminder.AppService.Users
 {
     public class AuthAppService : IAuthAppService
     {
-        private readonly IAuthRepository _authRepository;
+        private readonly IUserRepository _userRepository;
 
-        public AuthAppService(IAuthRepository authRepository)
+        public AuthAppService(IUserRepository authRepository)
         {
-            _authRepository = authRepository;
+            _userRepository = authRepository;
         }
 
         public async Task RegisterAsync(RegisterUserDto registerUserDto)
         {
             if (registerUserDto == null) throw new ArgumentNullException(nameof(registerUserDto));
 
-            var existingUser = await _authRepository.GetUserByEmailAsync(registerUserDto.Email);
+            var existingUser = await _userRepository.GetUserByEmailAsync(registerUserDto.Email);
             if (existingUser != null) throw new ArgumentException("Email já registrado");
 
             var user = new User(registerUserDto.Name, registerUserDto.Email, registerUserDto.Password);
-            await _authRepository.AddUserAsync(user);
+            await _userRepository.AddUserAsync(user);
         }
 
         public async Task<User> LoginAsync(LoginUserDto loginUserDto)
         {
             if (loginUserDto == null) throw new ArgumentNullException(nameof(loginUserDto));
 
-            var user = await _authRepository.GetUserByEmailAsync(loginUserDto.Email);
+            var user = await _userRepository.GetUserByEmailAsync(loginUserDto.Email);
             if (user == null || !user.VerifyPassword(loginUserDto.Password)) throw new UnauthorizedAccessException("Email ou senha inválidos.");
 
             return user;
