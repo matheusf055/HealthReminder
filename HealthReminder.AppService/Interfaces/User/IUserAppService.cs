16fed75efde0b516815ffe240afa5e8544df7b13
09042110 <09042110@FMN026-L101-015>
2025-02-24 19:35:51 -0300

    Cria o AppService de User e seus DTOs

---
+++ HealthReminder.AppService/Interfaces/User/IUserAppService.cs
@@ -0,0 +1,17 @@
+﻿using HealthReminder.AppService.User.DTOs;
+using HealthReminder.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthReminder.AppService.Interfaces.User
+{
+    public interface IUserAppService
+    {
+        Task<UserDetailsDto> GetUserDetailsDto(Guid id, IUser user);
+        Task UpdateUserAsync(UpdateUserDto updateUserDto, IUser user);
+        Task DeleteUserAsync(Guid id, IUser user);
+    }
+}
---
+++ HealthReminder.AppService/User/DTOs/UserDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthReminder.AppService.User.DTOs
+{
+    public class UserDetailsDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
